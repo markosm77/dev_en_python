46c786f6cad2211ab4d851ef5ab5489b283636cb
markosm77 <marko.manasterliou@gmail.com>
2022-10-10 12:23:16 +0200

    fin_du_cours

---
+++ cours_10_10_2022/premier_code.py
@@ -0,0 +1,77 @@
+# print("Hello, World")
+# if 5 > 2:
+#    print("cinq est supérieur à 2")
+# faut éviter d'avoir plus de 4 niveaux d'indentation
+# --------------------------------------------------------
+# print(2**1024)
+# --------------------------------------------------------
+x = 5
+y = "John"
+print(x)
+print(y)
+print(50 / 10)
+
+'''
+Opérateurs classiques:
+5<3
+5>3
+5>=3
+5<=3
+
+5==3
+5 is 3 
+
+5!=3
+
+SEPARATEUR \
+3+2-3.2 \
++3-15/26 \
+-15 + 3.2
+'''
+print(5 is 3)
+
+print(3 + 2 - 3.2 \
+      + 3 - 15 / 26 \
+      - 15 + 3.2)
+'''
+convention de notation :
+variable_1 = 10
+Variable_1 = 20 --> il faut éviter de mettre une majuscule au nom de la variable
+variable_1 = variable_2
+somme = variable_1 + variable_2
+L'affectation ne se fait que de la 
+'''
+
+variable_1 = 10
+variable_2 = 20
+print(f"var_1 : {variable_1}\nvar_2: {variable_2}")
+somme = variable_1 + variable_2
+print(somme)
+somme += 10
+print(somme)
+# permutation :
+variable_2, variable_1 = variable_1, variable_2
+print(f"var_1 : {variable_1}\nvar_2: {variable_2}")
+print(type(variable_1))
+'''
+Eléments pour afficher une chaîne:
+ma_chaine_2 = 'Exemple'
+ma_chaine_3 = "Exemple"
+'''
+ma_chaine_4 = '''Exemple'''
+ma_chaine_5 = """Exemple"""
+'''
+échapper un caractère:
+ma_chaine_6 'J\'aime le Python'
+ma_chaine_7 = "J'aime le Python"
+'''
+print(hex(2))
+print(bin(2))
+print(oct(2))
+
+# help(print) --> doc sur la méthode print()
+
+'''
+Exercice du jour:
+      Livrer le fichier du jour à artzvincent@gmail.com
+'''
